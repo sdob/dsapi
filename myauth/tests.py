df797ab4f96d66071cdc89934ebf3b7670882d80
sdob <sdob@cs.ucc.ie>
2016-01-05 15:43:05 +0000

    basic user registration stuff

--- myauth/tests.py
+++ myauth/tests.py
@@ -1,3 +1,4 @@
+import unittest
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from .factories import UserFactory
@@ -17,6 +18,33 @@ class UserTestCase(TestCase):
         user = UserFactory()
         self.assertTrue(Token.objects.filter(user=user).exists())
         self.assertEqual(Token.objects.filter(user=user).count(), 1)
+
+
+class CreateUserViewTestCase(APITestCase):
+
+    def test_sending_a_valid_email_and_password_works(self):
+        email = 'valid.email@example.com'
+        password = 'password'
+        data = {'email': email, 'password': password}
+        response = self.client.post(reverse('auth-create-user'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_registering_duplicate_email_fails(self):
+        email = 'valid.email@example.com'
+        password = 'password'
+        user = UserFactory(email=email)
+        data = {'email': email, 'password': password}
+        response = self.client.post(reverse('auth-create-user'), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # TODO: this should raise an exception somewhere, but I'll deal
+    # with it later
+    @unittest.skip('TODO...')
+    def test_password_is_required(self):
+        email = 'valid.email@example.com'
+        data = {'email': email}
+        response = self.client.post(reverse('auth-create-user'), data)
+
 
 class TokenAuthenticationTestCase(APITestCase):
     def test_sending_username_and_password_returns_token(self):
--- myauth/views.py
+++ myauth/views.py
@@ -1,9 +1,33 @@
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken as ObtainAuthTokenBase
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
 from profiles.models import Profile
+
+class ObtainAuthToken(ObtainAuthTokenBase):
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user': user.profile.id})
+
+
+class CreateUserView(APIView):
+
+    def post(self, request):
+        if User.objects.filter(email=request.data['email']).exists():
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        user = User.objects.create_user(
+                email=request.data['email'],
+                password=request.data['password']
+                )
+        token = Token.objects.get(user=user)
+        return Response({'token': token.key, 'user': user.profile.id})
+
 
 class CheckToken(APIView):
 
@@ -21,4 +45,6 @@ class CheckToken(APIView):
         except Profile.DoesNotExist:
             return default_bad_response
 
+create_user = CreateUserView.as_view()
 check_token = CheckToken.as_view()
+obtain_auth_token = ObtainAuthToken.as_view()
