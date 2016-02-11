b6cb155ad609920e14cbbcda76be93ed7dfb9ae9
sdob <sdob@cs.ucc.ie>
2016-02-11 17:40:46 +0000

    views for compressor comments

--- comments/views.py
+++ comments/views.py
@@ -7,6 +7,20 @@ from .models import DivesiteComment, SlipwayComment, CompressorComment
 from .serializers import DivesiteCommentSerializer, SlipwayCommentSerializer, CompressorCommentSerializer
 
 # Viewsets for comments.
+class CompressorCommentViewSet(viewsets.GenericViewSet,
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.DestroyModelMixin):
+
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    queryset = CompressorComment.objects.all()
+    serializer_class = CompressorCommentSerializer
+
+    def perform_create(self, serializer):
+        # Pull the slipway ID out of the request data
+        serializer.save(owner=self.request.user, compressor=Compressor.objects.get(id=self.request.data['compressor']))
+
 
 class DivesiteCommentViewSet(viewsets.GenericViewSet,
         mixins.CreateModelMixin,
