e6ad586e596de0cb4af60ab298c989927d2e137e
sdob <sdob@cs.ucc.ie>
2016-02-15 13:43:15 +0000

    register divesites app models with actstream

---
+++ divesites/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'divesites.apps.DivesitesConfig'
--- divesites/apps.py
+++ divesites/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class DivesitesConfig(AppConfig):
     name = 'divesites'
+
+    def ready(self):
+        from actstream import registry
+        registry.register(self.get_model('Compressor'))
+        registry.register(self.get_model('Divesite'))
+        registry.register(self.get_model('Dive'))
+        registry.register(self.get_model('Slipway'))
+
