5ba1433ce6a6020896648e41b86163e4f07c9200
sdob <sdob@cs.ucc.ie>
2016-02-23 19:37:52 +0000

    Allow blank captions

--- images/models.py
+++ images/models.py
@@ -8,7 +8,7 @@ class BaseImage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # Creation metadata
     owner = models.ForeignKey(User)
-    caption = models.TextField()
+    caption = models.TextField(blank=True)
     creation_date = models.DateTimeField(auto_now_add=True)
     # Images can be assigned as the 'header image' of their site
     is_header_image = models.BooleanField(default=False)
