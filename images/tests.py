f8d20eb01ebe53f6f3171fb02caf2fca5bc87b76
sdob <sdob@cs.ucc.ie>
2016-02-23 19:38:43 +0000

    Add some unit tests for Images

--- images/tests.py
+++ images/tests.py
@@ -1,7 +1,9 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from rest_framework import status
 from rest_framework.test import APITestCase
 from divesites.models import Compressor, Divesite, Slipway
-from divesites.factories import CompressorFactory, DivesiteFactory, SlipwayFactory
+from divesites.factories import CompressorFactory, DivesiteFactory, SlipwayFactory, UserFactory
 
 from images.models import CompressorImage, DivesiteImage, SlipwayImage
 
@@ -29,3 +31,47 @@ class SanityCheckTestCase(APITestCase):
         # Check that i2 is now a header image
         i2 = DivesiteImage.objects.get(id=i2.id)
         self.assertTrue(i2.is_header_image)
+
+
+class ImageCreateTestCase(APITestCase):
+
+    def setUp(self):
+        self.owner = UserFactory()
+        self.compressor = CompressorFactory(owner=self.owner)
+        self.ds = DivesiteFactory(owner=self.owner)
+        self.slipway = SlipwayFactory(owner=self.owner)
+
+    def test_site_owner_can_set_header_image(self):
+        self.client.force_authenticate(self.owner)
+        data = {
+                'divesite': self.ds.id,
+                'is_header_image': True
+                }
+        response = self.client.post(reverse('divesiteimage-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_only_site_owner_can_set_header_image(self):
+        u2 = UserFactory()
+        data = {
+                'divesite': self.ds.id,
+                'is_header_image': True
+                }
+        self.client.force_authenticate(u2)
+        response = self.client.post(reverse('divesiteimage-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response = self.client.post(reverse('compressorimage-list'), {
+            'compressor': self.compressor.id,
+            'is_header_image': True
+            })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response = self.client.post(reverse('slipwayimage-list'), {
+            'slipway': self.slipway.id,
+            'is_header_image': True
+            })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_only_site_owner_can_set_header_image_in_update(self):
+        u2 = UserFactory()
+        data = {
+
+                }
--- images/views.py
+++ images/views.py
@@ -1,3 +1,90 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from divesites.models import Compressor, Divesite, Slipway
+from divesites.permissions import IsOwnerOrReadOnly
+from .models import CompressorImage, DivesiteImage, SlipwayImage
+from .serializers import CompressorImageSerializer, DivesiteImageSerializer, SlipwayImageSerializer
 
-# Create your views here.
+class CompressorImageViewSet(viewsets.GenericViewSet,
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.DestroyModelMixin):
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    queryset = CompressorImage.objects.all()
+    serializer_class = CompressorImageSerializer
+
+    def check_compressor_ownership(self, compressor):
+        owner = self.request.user
+        # If we're trying to set the header and we don't own the compressor, raise an exception
+        is_header_image = self.request.data['is_header_image']
+        if is_header_image and not owner == compressor.owner:
+            raise PermissionDenied()
+
+    def perform_create(self, serializer):
+        owner = self.request.user
+        # Pull the compressor ID out of the request data
+        compressor = Compressor.objects.get(id=self.request.data['compressor'])
+        # Check site ownership
+        self.check_compressor_ownership(compressor)
+        # Save the instance
+        instance = serializer.save(owner=owner, compressor=compressor)
+
+
+class DivesiteImageViewSet(viewsets.GenericViewSet,
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.DestroyModelMixin):
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    queryset = DivesiteImage.objects.all()
+    serializer_class = DivesiteImageSerializer
+
+    def check_divesite_ownership(self, divesite):
+        owner = self.request.user
+        # If we're trying to set the header and we don't own the divesite, raise an exception
+        is_header_image = self.request.data['is_header_image']
+        if is_header_image and not owner == divesite.owner:
+            raise PermissionDenied()
+
+    def perform_create(self, serializer):
+        owner = self.request.user
+        # Pull the compressor ID out of the request data
+        divesite = Divesite.objects.get(id=self.request.data['divesite'])
+        # Check ownership
+        self.check_divesite_ownership(divesite)
+        # Save the instance
+        instance = serializer.save(owner=owner, divesite=divesite)
+
+
+class SlipwayImageViewSet(viewsets.GenericViewSet,
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.DestroyModelMixin):
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    queryset = SlipwayImage.objects.all()
+    serializer_class = SlipwayImageSerializer
+
+    def check_slipway_ownership(self, slipway):
+        owner = self.request.user
+        # If we're trying to set the header and we don't own the divesite, raise an exception
+        is_header_image = self.request.data['is_header_image']
+        if is_header_image and not owner == slipway.owner:
+            raise PermissionDenied()
+
+    def perform_create(self, serializer):
+        owner = self.request.user
+        # Pull the compressor ID out of the request data
+        slipway = Slipway.objects.get(id=self.request.data['slipway'])
+        # Check ownership
+        self.check_slipway_ownership(slipway)
+        # Save the instance
+        instance = serializer.save(owner=owner, slipway=slipway)
+
+    def perform_update(self, serializer):
+        # Check ownership
+        self.check_slipway_ownership(slipway)
+        return super(self, mixins.UpdateModelMixin).perform_update(serializer)
