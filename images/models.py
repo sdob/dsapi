c03ce4557eee2362ecf5c0b46c08055100ed341b
sdob <sdob@cs.ucc.ie>
2016-02-24 16:46:32 +0000

    Introduce an Image model with a generic foreign key

--- images/models.py
+++ images/models.py
@@ -2,6 +2,8 @@ import uuid
 from cloudinary.models import CloudinaryField
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from divesites.models import Compressor, Divesite, Slipway
 
 class BaseImage(models.Model):
@@ -13,6 +15,35 @@ class BaseImage(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     # Images can be assigned as the 'header image' of their site
     is_header_image = models.BooleanField(default=False)
+
+
+class Image(models.Model):
+    """
+    This is an image that belongs to a thing (probably a site, since
+    profile images are inherently different --- not content.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Foreign key to a content type, allowing a generic relation
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # ID of the object to which this image belongs
+    object_id = models.UUIDField()
+    # This generic foreign key allows us to link to any model
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    # The user who created this
+    owner = models.ForeignKey(User)
+    # A caption for the image
+    caption = models.TextField(blank=True)
+    # The actual 'image' field stores stuff from Cloudinary
+    image = CloudinaryField('image')
+    # When the image was added
+    creation_date = models.DateTimeField(auto_now_add=True)
+    # Images can be assigned as the 'header image' of their site,
+    # but only by the site's owner
+    is_header_image = models.BooleanField(default=False)
+
+
 
 
 # Create your models here.
