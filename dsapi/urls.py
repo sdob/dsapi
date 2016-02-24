c8b5481aa250d13e8920bfb674d479e6e3ea5db9
sdob <sdob@cs.ucc.ie>
2016-02-24 18:17:32 +0000

    Nest image URLs within compressor/divesite/serializer URLs

--- dsapi/urls.py
+++ dsapi/urls.py
@@ -18,30 +18,42 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from rest_framework.authtoken import views as authtoken_views
-from rest_framework.routers import DefaultRouter
+#from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import SimpleRouter, NestedSimpleRouter
 import comments.views
 import divesites.views
 import images.views
 import profiles.views
 from authviews.views import FacebookLogin, GoogleLogin
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(r'comments/compressors', comments.views.CompressorCommentViewSet)
 router.register(r'comments/divesites', comments.views.DivesiteCommentViewSet)
 router.register(r'comments/slipways', comments.views.SlipwayCommentViewSet)
 
-router.register(r'compressors', divesites.views.CompressorViewSet)
-router.register(r'divesites', divesites.views.DivesiteViewSet)
+router.register(r'compressors', divesites.views.CompressorViewSet, base_name='compressor')
+router.register(r'divesites', divesites.views.DivesiteViewSet, base_name='divesite')
 router.register(r'dives', divesites.views.DiveViewSet)
-router.register(r'slipways', divesites.views.SlipwayViewSet)
-router.register(r'users', profiles.views.ProfileViewSet)
+router.register(r'slipways', divesites.views.SlipwayViewSet, base_name='slipway')
+router.register(r'users', profiles.views.ProfileViewSet, base_name='profile')
+
+compressors_router = NestedSimpleRouter(router, r'compressors', lookup='compressor')
+compressors_router.register(r'images', images.views.ImageViewSet, base_name='compressor-image')
+
+divesites_router = NestedSimpleRouter(router, r'divesites', lookup='divesite')
+divesites_router.register(r'images', images.views.ImageViewSet, base_name='divesite-image')
+
+slipways_router = NestedSimpleRouter(router, r'slipways', lookup='slipway')
+slipways_router.register(r'images', images.views.ImageViewSet, base_name='slipway-image')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
+    url(r'^', include(compressors_router.urls)),
+    url(r'^', include(divesites_router.urls)),
+    url(r'^', include(slipways_router.urls)),
     url(r'^auth/', include('rest_auth.urls')),
     url(r'^auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
     url(r'^auth/google/$', GoogleLogin.as_view(), name='google_login'),
     url(r'^accounts/', include('allauth.socialaccount.urls')),
-    #url(r'^api-token-auth/', authtoken_views.obtain_auth_token, name='auth-endpoint'),
 ]
