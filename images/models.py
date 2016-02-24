68eae59cf2f0d30014c5f90c29d2202d690cd70a
sdob <sdob@cs.ucc.ie>
2016-02-24 18:27:08 +0000

    Limit the ContentTypes to which Images can have a ForeignKey

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 import uuid
 from cloudinary.models import CloudinaryField
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
@@ -23,9 +24,20 @@ class Image(models.Model):
     profile images are inherently different --- not content.
     """
 
+    # Limit the number of possible content types that Images can have
+    # as their foreign key
+    limit_choices = \
+            Q(app_label='divesites', model='compressor') | \
+            Q(app_label='divesites', model='divesite') | \
+            Q(app_label='divesites', model='slipway')
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # Foreign key to a content type, allowing a generic relation
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+            ContentType,
+            limit_choices_to=limit_choices,
+            on_delete=models.CASCADE
+            )
     # ID of the object to which this image belongs
     object_id = models.UUIDField()
     # This generic foreign key allows us to link to any model
