990180c626ae7d9b6fb314c7499c34a9fed5b838
sdob <sdob@cs.ucc.ie>
2016-02-25 17:27:30 +0000

    Add UserProfileImageSerializer

--- images/serializers.py
+++ images/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from profiles.serializers import MinimalProfileSerializer
-from .models import Image
+from .models import Image, UserProfileImage
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
     owner = MinimalProfileSerializer(source='owner.profile', read_only=True)
+
+
+class UserProfileImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfileImage
