09b9673f03a79414f3431f835c6280669933cfd4
sdob <sdob@cs.ucc.ie>
2016-04-09 13:32:14 +0100

    Add workaround for Django bug #11331 (opening new cache connection on every request)

--- dsapi/wsgi.py
+++ dsapi/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dsapi.settings")
 
 application = get_wsgi_application()
+
+# Fix Django closing connection to MemCachier after every request (#11331)
+from django.core.cache.backends.memcached import BaseMemcachedCache
+BaseMemcachedCache.close = lambda self, **kwargs: None
