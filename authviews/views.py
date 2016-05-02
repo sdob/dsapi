9d4ab02a52fe8d2a9ae66f88477ca6da39442215
sdob <sdob@cs.ucc.ie>
2016-05-02 13:45:30 +0100

    Add no-argument initalizers for OAuth2Adapters
    This commit is a workaround for the bug introduced with
    django-rest-auth and django-allauth > 0.25. See the following
    threads:
    http://stackoverflow.com/questions/36317325/django-rest-auth-facebooklogin-view-missing-1-required-positional-argument-r
    (SO thread)
    https://github.com/Tivix/django-rest-auth/issues/197 (bug and
    workaround)

--- authviews/views.py
+++ authviews/views.py
@@ -6,14 +6,29 @@ from rest_auth.registration.views import SocialLoginView
 
 from dsapi import settings
 
+# These custom classes with no-argument initializers
+# fix a bug with django-rest-auth and django-allauth > 0.25;
+# see the following:
+# https://github.com/Tivix/django-rest-auth/issues/197
+# http://stackoverflow.com/questions/36317325/django-rest-auth-facebooklogin-view-missing-1-required-positional-argument-r
+class FacebookOAuth2AdapterCustom(FacebookOAuth2Adapter):
+    def __init__(self):
+        pass
+
+
+class GoogleOAuth2AdapterCustom(GoogleOAuth2Adapter):
+    def __init__(self):
+        pass
+
+
 class FacebookLogin(SocialLoginView):
-    adapter_class = FacebookOAuth2Adapter
+    adapter_class = FacebookOAuth2AdapterCustom
     callback_url = settings.FACEBOOK_AUTH_CALLBACK_URL
     client_class = OAuth2Client
 
 
 class GoogleLogin(SocialLoginView):
-    adapter_class = GoogleOAuth2Adapter
+    adapter_class = GoogleOAuth2AdapterCustom
     callback_url = settings.GOOGLE_AUTH_CALLBACK_URL
     client_class = OAuth2Client
     # Try overriding social login
